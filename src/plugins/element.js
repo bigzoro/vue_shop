b8265e28207ce071cfad15e5bf1742c02b6e1bbc
xsdd123 <731618580@qq.com>
2021-09-17 01:15:10 +0800

    finished the rights function

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -23,7 +23,11 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-  MessageBox
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -48,6 +52,10 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 
 // 把Message挂载到Vue的原型对象上（￥message可以自定义名称），每个组件都可以通过this.$message获取 Message
 Vue.prototype.$message = Message
